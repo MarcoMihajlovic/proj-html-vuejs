30fc49f6f0df101fcd5ff9f7015ce82109dc9360
Marco <marcosinisa.mihajlovic@gmail.com>
2021-04-12 17:50:08 +0200

    Header Completato
    Con Jumbotron e Sale banner con pannello info creatore

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var app = new Vue ({
+    el: "#root",
+
+    data: {
+        logo_path: "img/MasterStudy-1.svg",
+        menu: ['Courses', 'Course Formats', 'Add Course', 'Pages', 'Demos'],
+        icons: ['fab fa-linkedin','fab fa-instagram', 'fab fa-twitter', 'fab fa-facebook']
+    }
+
+    
+})
