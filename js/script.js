dd5185f688166dba58130f06bf6c9d798a130357
Marco <marcosinisa.mihajlovic@gmail.com>
2021-04-14 15:32:56 +0200

    Aggiunte personali
    Varie card create utilizzando VueJs

--- js/script.js
+++ js/script.js
@@ -43,7 +43,89 @@ var app = new Vue ({
                     date: '-June 3, 2015' 
                 }
             ]
-        }
+        },
+
+        courseCards: [
+            {
+                img: 'img/photo-1496307042754-b4aa456c4a2d-370x200.jpeg',
+                description: 'How to be a DJ? Make Electronic Music',
+                type: 'Electronic',
+                level: 'Advanced',
+                lessions: '8 Lectures',
+                time: '6 Hours'
+            },
+
+            {
+                img: 'img/12345-1-370x200.png',
+                description: 'Nvidia and UE4 Technologies Practice',
+                type: 'Nvidia',
+                level: 'Advanced',
+                lessions: '8 Lectures',
+                time: '6 Hours'
+            },
+
+            {
+                img: 'img/photo-1491897554428-130a60dd4757-370x200.jpeg',
+                description: 'Fashion photography from professional',
+                type: 'Fashion',
+                level: 'Advanced',
+                lessions: '8 Lectures',
+                time: '6 Hours'
+            },
+
+            {
+                img: 'img/photo-1416339134316-0e91dc9ded92-370x200.jpeg',
+                description: 'Design Instruments for Communication',
+                type: 'Communication',
+                level: 'Intermediate',
+                lessions: '8 Lectures',
+                time: '6 Hours'
+            },
+
+            {
+                img: 'img/cathryn-lavery-67852-unsplash-370x200.jpg',
+                description: 'Make your Concept Right and Beautiful',
+                type: 'Art',
+                level: 'Intermediate',
+                lessions: '8 Lectures',
+                time: '6 Hours'
+            },
+
+            {
+                img: 'img/photo-1475452779376-caebfb988090-370x200.jpeg',
+                description: 'Road Bike Manual or How to Be a Champion',
+                type: 'Bicycling',
+                level: 'Beginner',
+                lessions: '8 Lectures',
+                time: '6 Hours'   
+            }
+        ],
+
+        whyBoxes: [
+            {
+                class: 'fas fa-suitcase',
+                name: 'Entrepreneurship',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, esse soluta? Magnam aperiam cupiditate, consequatur sequi culpa architecto at qui error, facere porro dolor, commodi perferendis maxime incidunt quis minima?'
+            },
+
+            {
+                class: 'fas fa-rocket',
+                name: 'Accelerated learning',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, esse soluta? Magnam aperiam cupiditate, consequatur sequi culpa architecto at qui error, facere porro dolor, commodi perferendis maxime incidunt quis minima?'
+            },
+
+            {
+                class: 'far fa-thumbs-up',
+                name: 'Productivity',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, esse soluta? Magnam aperiam cupiditate, consequatur sequi culpa architecto at qui error, facere porro dolor, commodi perferendis maxime incidunt quis minima?'
+            },
+
+            {
+                class: 'fas fa-cog',
+                name: 'Life Coaching',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, esse soluta? Magnam aperiam cupiditate, consequatur sequi culpa architecto at qui error, facere porro dolor, commodi perferendis maxime incidunt quis minima?'
+            }
+        ]
 
 
     }
