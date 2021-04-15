3cf1cbb65756a5694ed6b5a5d58d542067614d6e
Marco <marcosinisa.mihajlovic@gmail.com>
2021-04-15 11:28:06 +0200

    Secondo Footer
    Implementato un secondo modo per popolare il footer tenendo commentato il vecchio

--- js/script.js
+++ js/script.js
@@ -7,8 +7,48 @@ var app = new Vue ({
         menu: ['Courses', 'Course Formats', 'Add Course', 'Pages', 'Demos'],
         
         icons: ['fab fa-linkedin','fab fa-instagram', 'fab fa-twitter', 'fab fa-facebook'],
+
+        footerBoxes: [
+            {
+                name: 'ABOUT',
+                description: 'MasterStudy is Education WordPress theme featured by Learning Management System (LMS) for online education.',
+                developed: 'Developed by StylemixThemes',
+                icons: ['fab fa-facebook', 'fab fa-twitter', 'fab fa-instagram']
+            },
+
+            {
+                name: 'CONTACT',
+                address: 'USA, California, First Avenue, California',
+                tel: 'Tel.: +1 212 458 300 32',
+                fax: 'Fax: +1 212 375 24 14',
+                mail: 'info@masterstudy.com'
+            },
+
+            {
+                name: 'PAGES',
+                page: ['Blog', 'Home', 'Shortcodes', 'courses', 'Membership', 'Typography']
+            },
+
+            {
+                name: 'BLOG',
+                articles: [
+                    {
+                        img: 'img/photo-1517520287167-4bbf64a00d66-75x75.jpeg',
+                        title: 'Our main target is to "Developing Yourself as a Leader"',
+                        date: '-August 9, 2018'
+                    },
+                    
+                    {
+                        img: 'img/photo-1490376840453-5f616fbebe5b-75x75.jpeg',
+                        title: 'Those Other College Expenses You Aren\'t Thinking About',
+                        date: '-June 3, 2015' 
+                    }
+                ]
+            }
+
+        ],
         
-        about: {
+        /* about: {
             name: 'ABOUT',
             description: 'MasterStudy is Education WordPress theme featured by Learning Management System (LMS) for online education.',
             developed: 'Developed by StylemixThemes',
@@ -43,7 +83,7 @@ var app = new Vue ({
                     date: '-June 3, 2015' 
                 }
             ]
-        },
+        }, */
 
         courseCards: [
             {
